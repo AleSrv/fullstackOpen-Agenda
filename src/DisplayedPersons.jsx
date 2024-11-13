7af99f86128c083a245e230724bac749a34533e7
alesrv <srvf2019@gmail.com>
2024-11-13 16:33:11 +0100

    popover numeros estilo

--- src/DisplayedPersons.jsx
+++ src/DisplayedPersons.jsx
@@ -1,13 +1,42 @@
 import PropTypes from 'prop-types';
-
+import { useState, useRef, useEffect } from 'react';
 
 const DisplayedPersons = ({ displayedPersons }) => {
+    const [selectedPerson, setSelectedPerson] = useState(null);
+    const popoverRef = useRef(null);
+
+    const togglePopover = (person) => {
+        setSelectedPerson(selectedPerson === person.posicion ? null : person.posicion);
+    };
+
+    // Función para manejar el clic fuera del popover
+    const handleClickOutside = (event) => {
+        if (popoverRef.current && !popoverRef.current.contains(event.target)) {
+            setSelectedPerson(null);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     return (
-        <ul>
+        <ul style={{ position: 'relative' }}>
             {displayedPersons.map(person => (
-                <li key={person.posicion}>
+                <li key={person.posicion} onClick={() => togglePopover(person)} style={{ cursor: 'pointer', marginBottom: '10px' }}>
                     <strong>{person.posicion}- {person.name} {person.number}</strong>
+
+                    {selectedPerson === person.posicion && (
+                        <div ref={popoverRef} className="popoverDetails">
+                            <h3>Detalles de {person.name}</h3>
+                            <p><strong>Posición:</strong> {person.posicion}</p>
+                            <p><strong>Teléfono:</strong> {person.number}</p>
+                            <button onClick={() => setSelectedPerson(null)}>Cerrar</button>
+                        </div>
+                    )}
                 </li>
             ))}
         </ul>
@@ -17,9 +46,9 @@ const DisplayedPersons = ({ displayedPersons }) => {
 DisplayedPersons.propTypes = {
     displayedPersons: PropTypes.arrayOf(
         PropTypes.shape({
-            posicion: PropTypes.number,
-            name: PropTypes.string.isRequired, // name como string requerposiciono
-            number: PropTypes.string.isRequired // number como string requerposiciono
+            posicion: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            number: PropTypes.string.isRequired
         })
     ).isRequired
 };
